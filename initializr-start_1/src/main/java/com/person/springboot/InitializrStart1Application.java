394c11c715fef8019b5dc6ebe803900b6ea51d76
haogemao <507297824@qq.com>
2018-09-05 10:22:03 +0800

    fileUploadDemo
    单文件上传demo

---
+++ initializr-start_1/src/main/java/com/person/springboot/InitializrStart1Application.java
@@ -0,0 +1,12 @@
+package com.person.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitializrStart1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InitializrStart1Application.class, args);
+	}
+}
---
+++ spring_boot_upload/src/main/java/com/person/springboot/exception/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.person.springboot.exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.multipart.MultipartException;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+	@ExceptionHandler(value=MultipartException.class)
+	public String handler(MultipartException e, RedirectAttributes redirectAttributes) {
+		redirectAttributes.addFlashAttribute("message", e.getCause().getMessage());
+		return "redirect:/uploadStatus";
+	}
+}
